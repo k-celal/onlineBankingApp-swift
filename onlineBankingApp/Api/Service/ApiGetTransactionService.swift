64d128ecf4bf726a8b6de3fe08f80cf14f63602c
Celal Karahan <celal51kara@gmail.com>
2024-04-05 16:31:44 +0300

    [feat] api file write & error added

---
+++ onlineBankingApp/Api/Service/ApiGetTransactionService.swift
@@ -0,0 +1,65 @@
+//
+//  ApiGetTransactionService.swift
+//  onlineBankingApp
+//
+//  Created by Celal Karahan on 5.04.2024.
+//
+
+import Foundation
+class ApiGetTransactionService {
+    private let localStorageManager: LocalStorageManager
+    
+    init() {
+        self.localStorageManager = LocalStorageManager()
+    }
+    func getTransactionHistory(completion: @escaping (Result<TransactionHistoryResponse, Error>) -> Void) {
+        // Base URL
+        guard let url = URL(string: "http://127.0.0.1:8070/app/transaction_history") else {
+            completion(.failure(NetworksError.invalidURL))
+            return
+        }
+        
+        // Access token
+        guard let accessToken = localStorageManager.getAccessToken() else {
+            completion(.failure(NetworksError.missingAccessToken))
+            return
+        }
+        
+        // URLRequest oluştur
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        
+        // URLSession ile isteği gönder
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            // Hata kontrolü
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            // Yanıt kontrolü
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NetworksError.invalidResponse))
+                return
+            }
+            
+            // Yanıtın dönüştürülmesi
+            guard let data = data else {
+                completion(.failure(NetworksError.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let responseData = try decoder.decode(TransactionHistoryResponse.self, from: data)
+                completion(.success(responseData))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+
+}
--- onlineBankingApp/Utilities/errorUtil/NetworkError.swift
+++ onlineBankingApp/Utilities/errorUtil/NetworkError.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 enum NetworksError: Error {
+    case missingAccessToken
     case invalidURL
     case invalidResponse
     case statusCode(Int)
