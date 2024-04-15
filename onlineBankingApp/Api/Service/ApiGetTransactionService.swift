127d4c97dc1f7fddbb089bb5c78e7da5d379edac
Celal Karahan <celal51kara@gmail.com>
2024-04-15 19:40:39 +0300

    [feat] api get paymeny history func

--- onlineBankingApp/Api/Service/ApiGetTransactionService.swift
+++ onlineBankingApp/Api/Service/ApiGetTransactionService.swift
@@ -61,5 +61,54 @@ class ApiGetTransactionService {
         
         task.resume()
     }
+    func getPaymentHistory(completion: @escaping (Result<PaymentHistoryResponse, Error>) -> Void) {
+        // Base URL
+        guard let url = URL(string: "http://127.0.0.1:8070/app/payment_history") else {
+            completion(.failure(NetworksError.invalidURL))
+            return
+        }
+        
+        // Access token
+        guard let accessToken = localStorageManager.getAccessToken() else {
+            completion(.failure(NetworksError.missingAccessToken))
+            return
+        }
+        
+        // URLRequest oluştur
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        
+        // URLSession ile isteği gönder
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            // Hata kontrolü
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            // Yanıt kontrolü
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NetworksError.invalidResponse))
+                return
+            }
+            
+            // Yanıtın dönüştürülmesi
+            guard let data = data else {
+                completion(.failure(NetworksError.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let responseData = try decoder.decode(PaymentHistoryResponse.self, from: data)
+                completion(.success(responseData))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
 
 }
