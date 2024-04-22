63225faba030cdfb06356a03f2f3894de8643066
Celal Karahan <celal51kara@gmail.com>
2024-04-22 17:49:46 +0300

    [feat] Dashboard CollectionView

---
+++ onlineBankingApp/Utilities/Cells/DashboardCellsCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardCellsCollectionViewCell.swift
+//  onlineBankingApp
+//
+//  Created by Celal Karahan on 22.04.2024.
+//
+
+import UIKit
+
+class DashboardCellsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var account_ID: UILabel!
+    @IBOutlet weak var account_no: UILabel!
+    @IBOutlet weak var account_Name: UILabel!
+    @IBOutlet weak var account_Type: UILabel!
+    @IBOutlet weak var balance: UILabel!
+    
+    
+}
---
+++ onlineBankingApp/View/DashboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DashboardViewController.swift
+//  onlineBankingApp
+//
+//  Created by Celal Karahan on 22.04.2024.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    @IBOutlet weak var dashboardCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
