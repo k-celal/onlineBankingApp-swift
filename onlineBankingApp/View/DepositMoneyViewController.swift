57c457ba68949290d6fca2f40470ba4beaa3782b
Celal Karahan <celal51kara@gmail.com>
2024-04-23 15:32:57 +0300

    [feat] deposit money transaction

--- onlineBankingApp/View/DepositMoneyViewController.swift
+++ onlineBankingApp/View/DepositMoneyViewController.swift
@@ -15,5 +15,38 @@ class DepositMoneyViewController: UIViewController {
         
         
     }
-    
-}
+    @IBAction func depositMoneyButtonTapped(_ sender: UIButton) {
+            guard let amountString = depositAmountField.text else {
+                showAlert(message: "Lütfen bir miktar girin.")
+                return
+            }
+            
+            // Sayısal kontrol
+            guard let depositAmount = Double(amountString) else {
+                showAlert(message: "Lütfen geçerli bir miktar girin.")
+                return
+            }
+            
+            // API isteği
+            let apiService = ApiPostTransactionService()
+            apiService.depositMoney(accountId: "\(accountID ?? 0)", depositAmount: "\(depositAmount)") { result in
+                switch result {
+                case .success(let response):
+                    DispatchQueue.main.async {
+                        // API'den dönen mesajı ekrana bastır
+                        self.showAlert(message: response.message)
+                    }
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self.showAlert(message: error.localizedDescription)
+                    }
+                }
+            }
+        }
+        
+        func showAlert(message: String) {
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Tamam", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
