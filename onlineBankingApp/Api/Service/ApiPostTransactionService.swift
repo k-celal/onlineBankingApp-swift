4dea102d903e99c5fa7e3fe095d1d38cdf7e0476
Celal Karahan <celal51kara@gmail.com>
2024-04-24 15:01:30 +0300

    [feat] withdraw money api link

--- onlineBankingApp/Api/Service/ApiPostTransactionService.swift
+++ onlineBankingApp/Api/Service/ApiPostTransactionService.swift
@@ -6,7 +6,69 @@ class ApiPostTransactionService {
     init() {
         self.localStorageManager = LocalStorageManager()
     }
-    
+    func withdrawMoney(accountId: String, withdrawAmount: String, completion: @escaping (Result<WithdrawResponseModel, Error>) -> Void) {
+        guard let url = URL(string: "http://127.0.0.1:8070/transact/withdraw") else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Access token'i al
+        guard let accessToken = localStorageManager.getAccessToken() else {
+            completion(.failure(AuthenticationError.missingAccessToken))
+            return
+        }
+        
+        // Access token'i header'a ekle
+        request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        
+        // Request body oluştur
+        let withdrawRequestModel = WithdrawRequestModel(accountId: accountId, withdrawAmount: withdrawAmount)
+        do {
+            let encoder = JSONEncoder()
+            request.httpBody = try encoder.encode(withdrawRequestModel)
+        } catch {
+            completion(.failure(NetworksError.serializationError))
+            return
+        }
+        
+        // URLSession ile isteği gönder
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(NetworksError.invalidResponse))
+                return
+            }
+            
+            guard (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NetworksError.statusCode(httpResponse.statusCode)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworksError.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let responseData = try decoder.decode(WithdrawResponseModel.self, from: data)
+                completion(.success(responseData))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+
+        task.resume()
+
+    }
     func depositMoney(accountId: String, depositAmount: String, completion: @escaping (Result<DepositResponseModel, Error>) -> Void) {
         guard let url = URL(string: "http://127.0.0.1:8070/transact/deposit") else {
             completion(.failure(NetworkError.invalidURL))
